97771f57ceb082a8bd845814452d92ba248bc963
edicyl <parvful@gmail.com>
2023-03-10 05:02:56 +0900

    Initial commit

---
+++ Assignment 5/app/src/main/java/edu/temple/itemselector/SelectionActivity.kt
@@ -0,0 +1,13 @@
+package edu.temple.itemselector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectionActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selection)
+    }
+}
